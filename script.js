498412588ea0c5c0f60935d6b5666ab3b5a6b991
eglereact <eglereactjs@gmail.com>
2022-02-13 16:01:17 +0200

    make target words random every time

--- script.js
+++ script.js
@@ -7,10 +7,7 @@ const DANCE_ANIMATION_DURATION = 500;
 const keyboard = document.querySelector("[data-keyboard]");
 const alertContainer = document.querySelector("[data-alert-container]");
 const guessGrid = document.querySelector("[data-guess-grid]");
-const offsetFromDate = new Date(2022, 0, 1);
-const msOffset = Date.now() - offsetFromDate;
-const dayOffset = msOffset / 1000 / 60 / 60 / 24;
-const targetWord = targetWords[Math.floor(dayOffset)];
+const targetWord = targetWords[Math.floor(Math.random() * targetWords.length)];
 
 const startInteraction = () => {
   document.addEventListener("click", handleMouseClick);
